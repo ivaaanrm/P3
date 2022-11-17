28664d01f2b8b7c3467b711e426d2d1d836e3368
Nacho <nacho.sancho.p@gmail.com>
2022-11-17 12:44:52 +0100

    No prints

--- scripts/run_get_pitch.sh
+++ scripts/run_get_pitch.sh
@@ -4,8 +4,7 @@
 GETF0="get_pitch"
 
 for fwav in pitch_db/train/*.wav; do
-    ff0=${fwav/.wav/.f0}
-    echo "$GETF0 $fwav $ff0 ----"
+    ff0=${fwav/.wav/.fecho "$GETF0 $fwav $ff0 ----"
     $GETF0 $fwav $ff0 > /dev/null || (echo "Error in $GETF0 $fwav $ff0"; exit 1)
 done
 
